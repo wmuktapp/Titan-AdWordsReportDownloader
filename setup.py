1d7ccf55235b30d9c886be0be34d4bac6b172eb4
Adam Cunnington <adamcunnington.info@gmail.com>
2018-06-20 08:21:20 +0100

    Added auth details as command line options and refactored code flow to assemble yaml file from string

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     license="MIT",
     packages=setuptools.find_packages(),
     package_data={"": ["googleads.yaml"]},
-    install_requires=["click", "googleads"],
+    install_requires=["click", "googleads", "pyyaml"],
     entry_points={"console_scripts": ["adwordsreportdownloader = adwordsreportdownloader:main"]}
 )
